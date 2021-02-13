010292f39c8f2a267098cf200b4093120a4ba526
Leo Cheung <dear531@21cn.com>
2021-02-13 14:58:34 +0800

    string test

---
+++ string.lua
@@ -0,0 +1,17 @@
+#!/usr/bin/lua
+string1 = "this is string1"
+string2 = "this is string2"
+
+print(string1)
+print(string2)
+
+html = [[
+<html>
+<head></head>
+<dody>
+	<a herf="http://www.runoob.com/">菜鸟故事</a>
+</dody>
+</html>
+]]
+
+print(html)
