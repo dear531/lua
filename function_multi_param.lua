63fd00744b055caed295110170339de17367c20b
Leo Cheung <dear531@21cn.com>
2021-02-14 17:28:55 +0800

    function for multi params

---
+++ function_multi_param.lua
@@ -0,0 +1,14 @@
+#! /usr/bin/lua
+
+function average(...)
+	result = 0
+	local arg = {...}
+	for i,v in ipairs(arg) do
+		result = result + v;
+	end
+	--我们也可以通过 select("#",...) 来获取可变参数的数量:
+	print("总共传入" .. select("#", ...) .. "个参数");
+	return result/select("#", ...);
+end
+
+print("平均值为", average(10, 5, 3, 4, 5, 6));
